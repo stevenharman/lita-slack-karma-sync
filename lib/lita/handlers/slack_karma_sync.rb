d18b9ce5a460638fcac585ee234116f19b305ca2
Steven Harman <steven@harmanly.com>
2015-03-13 17:31:52 -0400

    Sync a user's karma term w/their UUID and name

--- lib/lita/handlers/slack_karma_sync.rb
+++ lib/lita/handlers/slack_karma_sync.rb
@@ -1,6 +1,75 @@
 module Lita
   module Handlers
     class SlackKarmaSync < Handler
+      TERMS_KEY = 'terms'.freeze
+      ANYNAME_GLOB = '*'.freeze
+
+      on :slack_user_created, :update_karma_terms
+
+      config :user_term_normalizer do
+        validate do |value|
+           t('callable_required') unless value.respond_to?(:call)
+        end
+      end
+
+      def update_karma_terms(payload)
+        user_id = payload.fetch(:user_id)
+
+        new_term = find_term(user_id)
+        existing_term = find_existing_term_for(user_id)
+        return if existing_term.eql?(new_term) # use #eql? b/c Term#== isn't properly aliased
+
+        copy_links(from: existing_term, to: new_term)
+        copy_karma(from: existing_term, to: new_term)
+        existing_term.delete
+      end
+
+      private
+
+      def find_existing_term_for(user_id)
+        #terms = karma_redis.zscan_each(TERMS_KEY, match: "@#{user_id} (*)").to_a
+        terms = karma_redis.zscan_each(TERMS_KEY, match: normalized_user_term(user_id)).to_a
+        terms = terms.map { |term, _score|
+          find_term(term, normalize: false)
+        }
+
+        fail "Found unexpected terms: #{}" if terms.size > 1
+
+        terms.first || NoMatchingTerm.new
+      end
+
+      def copy_karma(from:, to:)
+        karma_redis.zincrby(TERMS_KEY, from.own_score, to.to_s)
+      end
+
+      def copy_links(from:, to:)
+        from.links.each do |linked_term|
+          to.link(linked_term)
+        end
+      end
+
+      def find_term(raw_term, normalize: true)
+        Karma::Term.new(robot, raw_term, normalize: normalize)
+      end
+
+      def normalized_user_term(user_id)
+        config.user_term_normalizer.call(user_id, ANYNAME_GLOB)
+      end
+
+      def karma_redis
+        @karma_redis ||= Redis::Namespace.new('handlers:karma', redis: Lita.redis)
+      end
+
+      class NoMatchingTerm
+        def eql?(other)
+          self.class.equal?(other.class)
+        end
+        alias == eql?
+
+        def hash
+          self.class.hash
+        end
+      end
     end
 
     Lita.register_handler(SlackKarmaSync)
--- lita-slack-karma-sync.gemspec
+++ lita-slack-karma-sync.gemspec
@@ -14,7 +14,11 @@ Gem::Specification.new do |spec|
   spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
   spec.require_paths = ["lib"]
 
+  spec.required_ruby_version = ">= 2.1.0"
+
   spec.add_runtime_dependency "lita", ">= 4.2"
+  spec.add_runtime_dependency "lita-slack", ">= 1.1"
+  spec.add_runtime_dependency "lita-karma", ">= 3.0"
 
   spec.add_development_dependency "bundler", "~> 1.3"
   spec.add_development_dependency "rake"
